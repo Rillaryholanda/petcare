8bd1431367677f843658b6dac81c02415f3ac64a
Rillary Sousa <rillarysousa28@gmail.com>
2025-05-13 11:09:38 -0300

    UPDATE, seção duvida: HTML, CSS e JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* Selecione todos os elementos com a classe "duvida"*/
+
+const elementosDuvida = document.querySelectorAll(".duvida");
+
+elementosDuvida.forEach(function (duvida) {
+  duvida.addEventListener("click", function () {
+        // fecha as outras dúvidas
+        elementosDuvida.forEach(function (outraDuvida) {
+            if (outraDuvida !== duvida) {
+              outraDuvida.classList.remove("ativa");
+            }
+        });
+        //abre a dúvida
+    duvida.classList.toggle("ativa");
+  });
+});
